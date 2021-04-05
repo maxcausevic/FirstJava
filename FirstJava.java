572b3aa3a3df138eec85180ba4f59157182e0e86
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-04-04 19:00:10 -0500

    first commit

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Maxine Causevic");
+        System.out.println("I am 33 years old");
+        System.out.println("My hometown is Plainfield, IL");
+    }
+}
